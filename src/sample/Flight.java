d6ef4a61bf151edbae6f6513a3af7cc9e25c8bbb
nikos <nikos.evo@hotmail.com>
2021-04-23 19:23:59 +0200

    flight class added

---
+++ src/sample/Flight.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+public class Flight implements Serializable {
+
+    private Person seats[][] = {null}; //an double array that represents the seats of the plane
+    private int seatsAvailable;
+    private String id;
+    private String from;
+    private String to;
+    private LocalTime depart_time;
+
+    Flight(String id, String from, String to,LocalTime depart_time){
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.depart_time = depart_time;
+        seats = new Person[4][25];
+        Arrays.fill(seats,null);
+        seatsAvailable = 100; // since all the seats are available at the beginning
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Flight: " + id + " Departure at: " + depart_time + " Available seats: " + seatsAvailable;
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.LocalTime;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxmls/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1130, 530));
+        primaryStage.setScene(new Scene(root, 1130, 530 ));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+        System.out.println(new Flight("123","athens","to", LocalTime.parse("08:20")));
     }
 }
