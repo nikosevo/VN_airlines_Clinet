4f4c87f7824d2099256dc8d44059560da92e6b55
nikos <nikos.evo@hotmail.com>
2021-04-26 20:35:54 +0200

    now working for sure for sure....

--- src/sample/controllers/BookPageController.java
+++ src/sample/controllers/BookPageController.java
@@ -1,12 +1,6 @@
-package sample.contollers;
+package sample.controllers;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import sample.Handler;
-
-import java.io.IOException;
 
 public class BookPageController {
 
--- src/sample/controllers/FrontpageController.java
+++ src/sample/controllers/FrontpageController.java
@@ -1,4 +1,4 @@
-package sample.contollers;
+package sample.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
