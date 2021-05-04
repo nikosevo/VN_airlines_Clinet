e455e98d99d53054a8d3dbaf0c4b8f38adb68cd2
nikos <nikos.evo@hotmail.com>
2021-05-04 09:33:32 +0200

    mikroalages

--- src/sample/controllers/BookPageController.java
+++ src/sample/controllers/BookPageController.java
@@ -1,21 +1,30 @@
 package sample.controllers;
 
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import sample.Handler;
 import sample.Person;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
-public class BookPageController
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+
+public class BookPageController implements Initializable
 {
 
     @FXML
@@ -27,8 +36,19 @@ public class BookPageController
     private ArrayList<String> temporarilyNotAvailable;
     private ArrayList<Person> onlyForNewPersons = new ArrayList<Person>();
     protected String flightid;  //this is protected so that the fxml can see it
+    private boolean needWindowForNewPerson = false;
 
     //kinna like a constructor but is acrivated after the constructor so we can create the obj and then call this method
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(2), e -> {
+                    setGrid();
+                })
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
     @FXML
     public void initialize(Handler handler, String flightId)
     {
@@ -36,7 +56,10 @@ public class BookPageController
         this.flightid = flightId;
         setClicked();
         setGrid();
+
     }
+
+
 
     public void setGrid()
     {
@@ -92,6 +115,7 @@ public class BookPageController
                 //TODO THIS NEED TO BE FIXED FIRSTLY WE TAKE THE INFO FROM THE USER AND PROCEED ONLY WHEN HE HAS FILLED THE WINDOWS
                 //WITH HIS INFO
                 System.out.println(wishlist.size() + "size");
+                setGrid();
                 for (int i = 0; i < wishlist.size(); i++)
                 {
                     newpersonWindow(wishlist,i);
@@ -104,7 +128,6 @@ public class BookPageController
     {//nothing
         try
         {
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxmls/newPersonPage.fxml"));
             Parent root = loader.load();
             NewPersonPageController newperson = loader.getController();
@@ -113,8 +136,6 @@ public class BookPageController
             stage.setTitle("no other title");
             stage.setScene(new Scene(root, 500, 500));
             stage.show();
-
-
 
         } catch (IOException e)
         {
